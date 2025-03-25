6cfa7e5a6596a8f097a95f5d4d82e28d50ac41bd
Mikebestako <138659153+Mikebestako@users.noreply.github.com>
2025-03-25 17:31:55 +0800

    Add files via upload

---
+++ script/songs.js
@@ -0,0 +1,10 @@
+
+const burning = new Audio();
+const gohan = new Audio();
+const saliva = new Audio();
+const pod = new Audio();
+const wedd = new Audio();
+const electricBlue = new Audio();
+const timesUp = new Audio();
+
+
